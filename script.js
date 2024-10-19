d8d0f92ff5ff8bb4bd452bd42580dd2d01b4e18a
Kunal Sharma <159030119+kunalsharma-iitjmu@users.noreply.github.com>
2024-10-19 11:08:17 +0530

    ok

---
+++ script.js
@@ -0,0 +1,42 @@
+VanillaTilt.init(document.querySelectorAll(".box"),
+            {
+                max: 5,
+                speed: 200
+            });
+        VanillaTilt.init(document.querySelectorAll(".about-container"),
+            {
+                max: 5,
+                speed: 200
+            });
+        ScrollReveal().reveal('.text', {
+            duration: 2000,
+            origin: 'top',
+            distance: '100px',
+            reset: false,
+            delay: 200,
+            opacity: 0
+        });
+        ScrollReveal().reveal('.container', {
+            duration: 2000,
+            origin: 'left',
+            distance: '100px',
+            reset: false,
+            delay: 200,
+            opacity: 0
+        });
+        ScrollReveal().reveal('.container1', {
+            duration: 2000,
+            origin: 'top',
+            distance: '100px',
+            reset: false,
+            delay: 200,
+            opacity: 0
+        });
+        ScrollReveal().reveal('iframe', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '100px',
+            reset: false,
+            delay: 200,
+            opacity: 0
+        });
